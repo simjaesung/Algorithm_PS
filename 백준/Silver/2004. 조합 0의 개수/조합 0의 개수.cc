2f6430f238de8820b881e6249ab78d5431a08d8c
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-03-14 19:19:13 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	long long n, m, cnt5 = 0, cnt2 = 0;
+	cin >> n >> m;
+
+	for (long long i = 5; i <= n; i *= 5) cnt5 += n / i;
+	for (long long i = 2; i <= n; i *= 2) cnt2 += n / i;
+	
+	for (long long i = 5; i <= m; i *= 5) cnt5 -= m / i;
+	for (long long i = 2; i <= m; i *= 2) cnt2 -= m / i;
+	
+	for (long long i = 5; i <= n - m; i *= 5) cnt5 -= (n - m) / i;
+	for (long long i = 2; i <= n - m; i *= 2) cnt2 -= (n - m) / i;
+	
+	cout << min(cnt2, cnt5);
+	return 0;
+}
