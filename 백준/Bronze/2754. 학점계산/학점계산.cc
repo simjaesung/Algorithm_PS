4b85397c7115f76a961b024d029633d82fed57f5
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-03-17 23:48:49 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string s; cin >> s;
+	if (s == "A+") cout << "4.3";
+	if (s == "A0") cout << "4.0";
+	if (s == "A-") cout << "3.7";
+	if (s == "B+") cout << "3.3";
+	if (s == "B0") cout << "3.0";
+	if (s == "B-") cout << "2.7";
+	if (s == "C+") cout << "2.3";
+	if (s == "C0") cout << "2.0";
+	if (s == "C-") cout << "1.7";
+	if (s == "D+") cout << "1.3";
+	if (s == "D0") cout << "1.0";
+	if (s == "D-") cout << "0.7";
+	if (s == "F") cout << "0.0";
+	
+	
+	
+	
+	return 0;
+}
