e5369820733629911c7e6911ea6672004fca3a25
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-03-22 12:14:39 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n,k,c;
+vector<pair<int,int>>v;
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> k;
+    
+    for(int i = 0; i< n; i++){
+        int g,s,d,val;
+        cin >> c >> g >> s >> d;
+        val = 100 * g + 10 * s + d;
+        v.push_back({val,c});
+    }
+    
+    sort(v.begin(), v.end());
+    
+    int ans = 1;
+    for(int i = v.size() - 2; i >= 0; i-- ){
+        if(v[i+1].first != v[i].first) ans++;
+        if(v[i].second == k) {
+            cout << ans;
+            return 0;
+        }
+    }
+    return 0;
+}
