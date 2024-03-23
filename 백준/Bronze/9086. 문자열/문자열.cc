9ec029f643ac635e2780fae00f6cd99b26af7f2b
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-03-24 02:31:33 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n; cin >> n;
+	while (n--) {
+		string s; cin >> s;
+		cout << s[0] << s[s.length() - 1] << '\n';
+	}
+	
+	return 0;
+}
