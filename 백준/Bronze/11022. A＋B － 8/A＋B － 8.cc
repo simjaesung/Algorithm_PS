9c541bec3e141c0af279c5260de086d56f561162
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-04-11 12:56:19 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, A, B;
+	cin >> n;
+	int i(0);
+
+	while (i++ < n) {
+		cin >> A >> B;
+		cout << "Case #" << i << ": " 
+			<< A << " + "<<B <<" = "<< A + B << endl;
+	}
+
+	return 0;
+}
