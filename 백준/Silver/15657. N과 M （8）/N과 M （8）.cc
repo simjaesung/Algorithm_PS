c326a56480c3e65f01111ab27e9301b0dfec290e
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-04-14 19:20:42 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int num[10], a[10];
+
+void go(int index, int start, int n, int m)
+{
+	if (index == m) {
+		for (int i = 0; i < m; i++)
+			cout << num[a[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i < n; i++) {
+		a[index] = i;
+		go(index + 1, i, n, m);
+	}
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr); cout.tie(nullptr);
+	int n, m; cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> num[i];
+
+	sort(num, num + n);
+	go(0, 0, n, m);
+
+	return 0;
+}
