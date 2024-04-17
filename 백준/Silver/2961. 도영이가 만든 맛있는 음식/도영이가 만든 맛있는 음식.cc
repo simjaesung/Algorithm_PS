e7a350788686f538838fab55fe770da1406acb52
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-04-17 13:43:56 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -3,40 +3,28 @@
 using namespace std;
 typedef long long ll;
 
-int n, arr[15][2], c[15];
+int n, arr[15][2];
 ll ans = LLONG_MAX;
-
-//a는 신맛 b는 쓴맛
-void go(ll a, ll b)
-{
-    if(a > 0 && b > 0) ans = min(abs(a-b),ans);
-    
-    for(int i = 0; i < n; i++) {
-        ll shin = arr[i][0];
-        ll ssn = arr[i][1];
-        if(c[i]) continue;
-        
-        c[i] = 1;
-        a *= shin;
-        b += ssn;
-        go(a,b);
-        
-        c[i] = 0;
-        a /= shin;
-        b -= ssn;
-    }
-}
-
 
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n;
+    
     for(int i = 0; i < n; i++){
         cin >> arr[i][0] >> arr[i][1];
     }
     
-    go(1,0);
+    for(int i = 1; i<(1<<n); i++){
+        ll a = 1, b = 0;
+        for(int k = 0; k < n; k++){
+            if(i & (1 << k)){
+                a *= arr[k][0];
+                b += arr[k][1];
+            }
+        }
+        ans = min(abs(a-b),ans);
+    }
     cout << ans;
     return 0;
 }
