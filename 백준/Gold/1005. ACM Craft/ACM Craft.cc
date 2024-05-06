cc5b63249e7d85172d0afc2f99c475921dbc0604
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-05-06 15:26:20 +0900

    [Gold III] Title: ACM Craft, Time: 168 ms, Memory: 2816 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -4,9 +4,6 @@
 #include<algorithm>
 using namespace std;
 int t,n,m,k,a,b,last;
-int tim[1005]; //건물 짓는데 걸리는 시간
-int c[1005]; //선행건물 개수
-vector<int>p[1005]; //선행 관계
 
 int main()
 {
@@ -14,9 +11,10 @@ int main()
     cin >> t;
     while(t--){
         cin >> n >> m;
-        fill(tim,tim+1005,0);
-        fill(c,c+1005,0);
-        for(int i = 1; i<=n; i++) p[i].clear();
+        int tim[1005] = {0,};//건물 짓는데 걸리는 시간
+        int c[1005] = {0,}; //선행건물 개수
+        int tot[1005] = {0,}; //총걸리는 시간
+        vector<int>p[1005]; //선행 관계
         
         for(int i = 1; i<=n; i++){
             cin >> k; tim[i] = k;
@@ -37,46 +35,14 @@ int main()
         }
         
         while(!q.empty()){
-            int sz = q.size();
-            int check = 0; // 마지막 건물이 나왔는지 체크
-            int timetmp = 0; //0시간 체크
-            for(int i = 0; i<sz; i++){
-                int tmp = q.front();
-                q.pop();
-                if(tim[tmp]) timetmp++;
-                tim[tmp]--;
-                q.push(tmp);
+            int tmp = q.front(); q.pop();
+            for(auto x : p[tmp]){
+                tot[x] = max(tot[x], tim[tmp] + tot[tmp]);
+                c[x]--;
+                if(!c[x]) q.push(x);
             }
-            
-            //짓는 시간이 0시간인 건물이 존재할 경우 다른 건물들의 시간 복구
-            if(timetmp / sz < 1){
-                for(int i = 0; i<sz; i++){
-                    int tmp = q.front();
-                    q.pop();
-                    if(!tim[tmp]) tim[tmp]++;
-                    q.push(tmp);
-                }
-            }
-            else ans++;
-            
-            for(int i = 0; i<sz; i++){
-                int tmp = q.front();
-                q.pop();
-                if(tim[tmp] <= 0){
-                    if(tmp == last) {
-                        check = 1;
-                        break;
-                    }
-                    for(auto x : p[tmp]){
-                        c[x]--;
-                        if(!c[x]) q.push(x);
-                    }
-                }
-                else q.push(tmp);
-            }
-            if(check) break;
         }
-        cout << ans <<'\n';
+        cout << tot[last] + tim[last] <<'\n';
     }
     return 0;
 }
