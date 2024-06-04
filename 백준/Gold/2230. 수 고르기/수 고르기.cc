63bf90acfaef3a0e39ce35fbb231d5af8f40aacb
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-06-04 18:37:56 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,27 +1,26 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 #include<limits.h>
 using namespace std;
+int n,m,ans = INT_MAX;
+int arr[100005];
 
-int n, m, val, s, e, ans = INT_MAX;
-vector<int>v;
 int main()
 {
-	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> val; v.push_back(val);
-	}
-	sort(v.begin(), v.end());
-
-	while (s < n && e < n) {
-		if (v[e] - v[s] < m) e++;
-		else {
-			ans = min(ans, v[e] - v[s]);
-			s++;
-		}
-	}
-	cout << ans;
-	return 0;
+    ios::sync_with_stdio(0); cin.tie(0);    
+    cin >> n >> m;
+    for(int i = 0; i<n; i++) cin >> arr[i];
+    sort(arr,arr+n);
+    int s = 0; int e = 0;
+    while(s < n && e < n){
+        if(s == e) e++;
+        else if(arr[e] - arr[s] >= m){
+            ans = min(arr[e]-arr[s],ans);
+            s++;
+        }
+        else e++;
+    }
+    cout << ans;
+    return 0;
 }
