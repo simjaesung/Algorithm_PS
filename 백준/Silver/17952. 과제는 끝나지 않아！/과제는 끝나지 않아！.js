5e033fb2b45cdb5afa962399c240cdd2241b9de8
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-06-22 01:53:25 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1680 ms, Memory: 206292 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.js
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.js
@@ -1,38 +1,20 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map((el)=>el.split(' ').map((item)=>+item));
 let n = input.shift();
 
-class Stack {
-	constructor() {
-		this._arr = [];
-	}
-	push(item) {
-		this._arr.push(item);
-	}
-	pop() {
-		return this._arr.pop();
-	}
-	top() {
-		return this._arr[this._arr.length - 1];
-	}
-	size() {
-		return this._arr.length;
-	}
-	isEmpty(){
-		return this.size() == 0;
-	}
-}
-const s = new Stack();
-
 function sol(arr){
 	let ans = 0;
+	let s = [];
 	for(let i = 0; i<arr.length; i++){
 		if(arr[i].length == 1){
-			if(s.isEmpty()) continue;
+			if(s.length == 0) continue;
 			else{
-				let[a,b] = s.top();
-				s.pop();
-				if(b-1 == 0) ans += a;
-				else s.push([a,b-1])
+				let[a,b] = s[s.length-1];
+				
+				if(b-1 == 0) {
+					ans += a;
+					s.pop();
+				}
+				else s[s.length-1][1]--;
 			}
 		}
 		else{
