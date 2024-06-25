dfad7453b386876751582b94d2c41522212d67e7
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-06-25 23:07:41 +0900

    [Silver V] Title: 기상캐스터, Time: 192 ms, Memory: 10584 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.js
@@ -0,0 +1,18 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [h,w] = input.shift().split(' ').map((item)=>+item);
+function solution(){
+	for(let i = 0; i<h; i++){
+		let tmp = input[i].split('');
+		let rain = -1;
+		let el = "";
+		for(let j = 0; j<w; j++){
+			if(tmp[j] == 'c') rain = 0;
+			else if(tmp[j] == '.'){
+				if(rain != -1) rain++; 
+			}
+			el+=rain+' ';
+		}
+		console.log(el);
+	}
+}
+solution();
