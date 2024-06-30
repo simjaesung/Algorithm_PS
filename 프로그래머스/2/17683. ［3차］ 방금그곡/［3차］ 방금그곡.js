ba30ef545f7e1f02dc6625636753612a70d48be4
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-06-30 13:02:33 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -0,0 +1,59 @@
+function rmPlat(s){
+    s = s.replaceAll("C#","c");
+    s = s.replaceAll("D#","d");
+    s = s.replaceAll("F#","f");
+    s = s.replaceAll("G#","g");
+    s = s.replaceAll("A#","a");
+    s = s.replaceAll("B#","b");
+    return s;
+}
+
+
+function k(info){
+    info = info.split(',');
+    let tmp = [];
+    info[3] = rmPlat(info[3]);
+    let len = info[3].length;
+    
+    let hour = ( (+info[1].slice(0,2)) - (+info[0].slice(0,2))) * 60;
+    let min = (+info[1].slice(3)) - (+info[0].slice(3));
+    let time = hour + min;
+    
+    let sound = "";
+    
+    let a = Math.floor(time / len);
+    let b = time % len;
+    
+    for(let i = 0; i<a; i++) sound += info[3];
+    for(let i = 0; i<b; i++) sound += info[3][i];
+    
+    tmp.push(time, info[2], sound);
+    
+    return tmp;
+}
+
+function compare(a,b){
+    if(a[0] != b[0]) return b[0] - a[0];
+}
+
+function solution(m, musicinfos) {
+    m = rmPlat(m);
+    
+    var answer = '(None)';
+    
+    let music = [];
+    
+    musicinfos.forEach((e,i)=>{
+        music.push(k(e));
+    })
+    
+    music.sort(compare);
+    
+    music.forEach((e,i)=>{
+        if(e[2].includes(m)) {
+            if(answer === '(None)') answer = e[1];
+        }
+    })
+    
+    return answer;
+}
