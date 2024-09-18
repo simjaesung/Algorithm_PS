069526a753d5ac44fe27a96c9e74c4d391aa8be1
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-09-18 23:30:36 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 5228 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int arr[1005], ans = 0;
+vector<int>v;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) v.push_back(arr[i] + arr[j]);
+	}
+	sort(v.begin(), v.end());
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (binary_search(v.begin(), v.end(), arr[i] - arr[j])) {
+				cout << arr[i]; return 0;
+			}
+		}
+	}
+	
+	cout << ans;
+	return 0;
+}
