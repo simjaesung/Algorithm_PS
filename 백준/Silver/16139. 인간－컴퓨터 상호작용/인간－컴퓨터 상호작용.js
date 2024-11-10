542de15fd095a8e4af57e1a235670e964e30e83c
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-11-11 00:34:19 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 668 ms, Memory: 139704 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -1,31 +1,26 @@
 let input = require('fs').readFileSync(0, 'utf-8').toString().trim().split('\n');
-const S = input[0];
-const n = +input[1];
-const obj = new Array(S.length).fill({});
+const [S,n,...data] = input;
+const obj = {};
+obj[-1] = new Array(26).fill(0);
 
 const init = () => {
-	obj[0][S[0]] = 1;
-	for(let i = 1; i<S.length; i++) {
-		let tmp = {...obj[i-1]};
-		if(tmp[S[i]]) tmp[S[i]]++;
-		else tmp[S[i]] = 1;
-		obj[i] = tmp;
+	for(let i = 0; i<S.length; i++) {
+		//obj[i] = [...obj[i-1]];
+		obj[i] = obj[i - 1].slice();
+		const c = S[i].charCodeAt() - 97;
+		obj[i][c]++;
 	}
 }
 
 const sol = () => {
 	init();
-	for(let i = 2; i<2+n; i++){
-		const [a,l,s] = input[i].split(' ');
-		const [tmp1,tmp2] = [obj[l], obj[s]];
-		if(tmp1[a]) {
-			if(S[l] === a) console.log(tmp2[a] - tmp1[a] + 1);
-			else console.log(tmp2[a] - tmp1[a]);
-		} else{
-			if(tmp2[a]) console.log(tmp2[a]);
-			else console.log(0);
-		}
+	const result = [];
+	for(let i = 0; i<n; i++){
+		const [a,l,r] = data[i].split(' ').map(x => !isNaN(x)? +x : x);
+		const c = a.charCodeAt() - 97;
+		result.push(obj[r][c] - obj[l-1][c]);
 	}
+	console.log(result.join('\n'));
 }
 
 sol();
