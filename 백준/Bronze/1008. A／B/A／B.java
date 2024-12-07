72330e072312b0433f87dc48e0a34d5db20f93af
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-12-07 15:19:41 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		double a = scanner.nextInt();
+		double b = scanner.nextInt();
+		System.out.println(a/b);
+	}
+}
