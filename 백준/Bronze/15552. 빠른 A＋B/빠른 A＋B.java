698b56cc2d3ffd3b11d7a1a9260647efe8ebe9aa
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-12-10 23:23:23 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 832 ms, Memory: 278444 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,15 +1,19 @@
 import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(bf.readLine());
-        for(int i = 0; i < n; i++){
-            String s = bf.readLine();
-            int a = Integer.parseInt(s.split(" ")[0]);
-            int b = Integer.parseInt(s.split(" ")[1]);
-            bw.write(a+b+"\n");
-        }
-        bw.flush();
-    }
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(n-- > 0){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(Integer.toString(a + b) + "\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
