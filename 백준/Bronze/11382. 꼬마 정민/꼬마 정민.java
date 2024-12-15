94c5d9e536359476ca2d8864c817ac704a648fa0
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-12-16 01:18:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		long ans = a + b + c;
+		
+		System.out.println(ans);
+	}
+}
