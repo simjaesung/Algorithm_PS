130fb0eaf123d6f0a35e22b8eb4c70b488f6c3d2
simjaesung <89987940+simjaesung@users.noreply.github.com>
2024-12-18 23:15:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println((int)s.charAt(0));
+	}
+}
