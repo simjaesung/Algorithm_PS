05e83c8486e5433094b8df7ec72844785e01babe
simjaesung <89987940+simjaesung@users.noreply.github.com>
2025-02-08 01:32:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 812 ms, Memory: 125596 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,20 +9,14 @@ class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		int[][] arr = new int[n + 1][n + 1];
+		int[][] dp = new int[n + 1][n + 1];
 		for(int i = 1; i <= n; i++){
 			StringTokenizer st2 = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= n; j++){
-				arr[i][j] = Integer.parseInt(st2.nextToken());
-			}
-		}
-		
-		int[][] dp = new int[n + 1][n + 1];
-		for(int i = 1; i <= n; i++){
-			for(int j = 1; j <= n; j++){
-				if(i == 1) dp[i][j] = dp[i][j-1] + arr[i][j];
-				else if(j == 1) dp[i][j] = dp[i-1][j] + arr[i][j];
-				else dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i][j] - dp[i-1][j-1];
+				int k = Integer.parseInt(st2.nextToken());
+				if(i == 1) dp[i][j] = dp[i][j-1] + k;
+				else if(j == 1) dp[i][j] = dp[i-1][j] + k;
+				else dp[i][j] = dp[i-1][j] + dp[i][j-1] + k - dp[i-1][j-1];
 			}
 		}
 		
