7d9afafcfa0ad00de0bc1c590e71db70ed36bd90
simjaesung <89987940+simjaesung@users.noreply.github.com>
2025-02-16 15:31:54 +0900

    [Silver II] Title: 과일 탕후루, Time: 324 ms, Memory: 30884 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		int[] cnt = new int[10]; //과일 개수
+		for(int i = 0; i < n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int start = 0;
+		int end = 0;
+		int ans = 0;
+		
+		while(end < n){
+			while(start < n){
+				cnt[arr[start++]]++;
+				if(checkF(cnt) > 2){
+					cnt[arr[--start]]--;
+					break;
+				}
+			}
+			ans = Math.max(ans, start - end);
+			cnt[arr[end++]]--;
+		}
+		
+		System.out.println(ans);
+	}
+	
+	public static int checkF(int[] cnt){
+		int c = 0;
+		for(int f : cnt){
+			if(f > 0) c++;
+		}
+		return c;
+	}
+}
