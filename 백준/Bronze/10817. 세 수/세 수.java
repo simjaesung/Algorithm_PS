b16b4595102e2dc4a15dffd9f8fe56ff4632f682
simjaesung <89987940+simjaesung@users.noreply.github.com>
2025-05-05 16:18:52 +0900

    [Bronze III] Title: 세 수, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[3];
+		for(int i = 0; i < 3; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+	}
+}
