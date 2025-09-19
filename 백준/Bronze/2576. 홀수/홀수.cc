08aa00d0e6176ed462e3c68d2b35fb5efc175660
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-20 00:14:29 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.cc
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -1,23 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
-int main()
-{
-	int arr[7];
-	int sum = 0, ans = -1;
-
-	for (int i = 0; i < 7; i++) {
-		cin >> arr[i];
-		if (arr[i] % 2 == 1) {
-			if (ans != -1) ans = min(ans, arr[i]);
-			else ans = arr[i];
-			sum += arr[i];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int minVal = 100;
+	int val, sum = 0;
+	for(int i = 0; i < 7; i++){
+		cin >> val;
+		if(val % 2 == 1){
+			sum += val;
+			minVal = min(val,minVal);
 		}
 	}
-	if (sum > 0)cout << sum << '\n' << ans;
-	else cout << ans;
 
+	if(sum == 0){
+		cout << -1;
+		return 0;
+	}
 
-
+	cout << sum << "\n" << minVal;
 	return 0;
+	
 }
