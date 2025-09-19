62775435eb86a45a11c2108d84916da6da080030
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-20 00:10:31 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -4,20 +4,15 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int a,b,c,d;
+	int input, result;
+	string res = "DCBAE";
 
 	for(int i = 0; i < 3; i++){
-		int cnt = 0;
-		cin >> a >> b >> c >> d;
-		if(a) cnt++;
-		if(b) cnt++;
-		if(c) cnt++;
-		if(d) cnt++;
-
-		if(cnt == 4) cout << "E" << "\n";
-		else if(cnt == 3) cout << "A" << "\n";
-		else if(cnt == 2) cout << "B" << "\n";
-		else if(cnt == 1) cout << "C" << "\n";
-		else cout << "D" << "\n";
+		result = 0;
+		for(int k = 0; k < 4; k++){
+			cin >> input;
+			result += input;
+		}
+		cout << res[result] << "\n";
 	}
 }
