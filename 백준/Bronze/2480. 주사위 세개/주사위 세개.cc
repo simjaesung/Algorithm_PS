4506995c67c9dbc4576e9a0e3fe9b847e1ce3167
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-20 00:00:33 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,28 +1,15 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
-int main()
-{
-	int a, b, c;
-	cin >> a >> b >> c;
-
-	if (a == b && b == c) {
-		cout << 10000 + a * 1000;
-	}	
-	else if ((a == b && a != c) || (a == c && a != b)){
-		cout << 1000 + a * 100;
-	}
-	else if ((b == c && b != a)) {
-		cout << 1000 + b * 100;
-	}
-	else {
-		int tmp = a;
-		if (b > tmp) tmp = b;
-		if (c > tmp) tmp = c;
-
-		cout << tmp * 100;
-	}
-
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	
+	int a,b,c;
+	cin >> a >> b >> c;
+	if(a == b && b == c) cout << 10000 + a * 1000;
+	else if(a == b || a == c) cout << 1000 + a * 100;
+	else if(b == c) cout << 1000 + b * 100;
+	else cout << max({a,b,c}) * 100;
 	return 0;
 }
