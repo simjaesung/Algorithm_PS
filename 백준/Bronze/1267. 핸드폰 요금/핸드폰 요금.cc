33767e93586c8affb803050adb4d84baed9334e4
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-20 11:55:49 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -11,11 +11,8 @@ int main() {
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> time;
-		if(time < 30) y += 10;
-		else y += (time / 30 + 1) * 10;
-
-		if(time < 60) m += 15;
-		else m += (time / 60 + 1) * 15;
+		y += (time / 30 + 1) * 10;
+		m += (time / 60 + 1) * 15;
 	}
 
 	if(y == m) cout << "Y M " << y;
