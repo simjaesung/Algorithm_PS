ae398a6c961f87ecf30f5a43d34b4bd9ebbcef4e
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-20 12:07:25 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -1,17 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main()
-{
-	int card[21];
-	for (int i = 1; i <= 20; i++) card[i] = i;
-
-	int a,b,n = 10;
-
-	while (n--) {
-		cin >> a >> b;
-		for (int i = 0; i <= (b - a) / 2; i++) swap(card[a + i], card[b - i]);
-	}
-	for (int i = 1; i <= 20; i++) cout << card[i] << ' ';
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	
+	int arr[20];
+	for(int i = 1; i <= 20; i++) arr[i-1] = i;
+
+	int a,b;
+	for(int i = 0; i < 10; i++){
+		cin >> a >> b;
+		while(a < b){
+			swap(arr[a-1], arr[b-1]);
+			a++; b--;
+		}
+	}
+
+	for(int i = 1; i <= 20; i++){
+		cout << arr[i - 1] << " ";
+	}
+
 	return 0;
 }
