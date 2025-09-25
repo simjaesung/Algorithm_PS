ace173714daa3d03aec736a1788fec447ec9e9f6
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-25 23:53:38 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,23 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-typedef long long ll;
+using ll = long long;
 
-ll mod(ll a, ll b, ll c) 
-{
-	if (b == 1) return a % c;
-	ll val = mod(a, b / 2, c);
+ll func(ll a, ll b, ll c){
+	if(b == 1) return a % c;
+	ll val = func(a, b/2, c);
 	val = val * val % c;
-	if (b % 2 == 1) return val * a % c;
-	else return val;
+	if(b % 2 == 0) return val;
+	return val * a % c;
 }
 
-
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
-	ll a, b, c;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	ll a,b,c;
 	cin >> a >> b >> c;
-
-	cout << mod(a, b, c);
+	cout << func(a,b,c);
 	return 0;
 }
