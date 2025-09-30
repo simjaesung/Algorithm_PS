e77f05a0ca0a58f893956e730005a03e82de3205
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-30 23:09:04 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -1,33 +1,29 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int num[10], a[10];
+int n,m;
+int arr[10], memo[10];
 
-void go(int index, int start, int n, int m)
-{
-	if (index == m) {
-		for (int i = 0; i < m; i++)
-			cout << num[a[i]] << ' ';
-		cout << '\n';
+void func(int k, int idx){
+	if(k == m){
+		for(int i = 0; i < m; i++){
+			cout << memo[i] << " ";
+		}
+		cout << "\n";
 		return;
 	}
-
-	for (int i = start; i < n; i++) {
-		a[index] = i;
-		go(index + 1, i, n, m);
+	for(int i = idx; i < n; i++){
+		memo[k] = arr[i];
+		func(k + 1, i);
 	}
-
 }
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(nullptr); cout.tie(nullptr);
-	int n, m; cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> num[i];
 
-	sort(num, num + n);
-	go(0, 0, n, m);
-
+int main() {
+	char input[100];
+	cin >> n >> m;
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	func(0, 0);
 	return 0;
 }
