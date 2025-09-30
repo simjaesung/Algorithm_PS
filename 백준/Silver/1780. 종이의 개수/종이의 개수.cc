c5cea230d40147dec40783914ee0054d253afc8e
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-09-30 23:43:26 +0900

    [Silver II] Title: 종이의 개수, Time: 376 ms, Memory: 99676 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,49 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+int arr[5000][5000];
+int n;
+int ans_zero, ans_one, ans_mone;
 
-int arr[2200][2200], ans[3];
+void input(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin >> arr[i][j];
+		}
+	}
+}
 
-bool check(int a, int b,int n) {
-	int val = arr[a][b];
-	for (int i = a; i < a+n; i++) {
-		for (int j =b; j < b+n; j++) {
-			if (val != arr[i][j]) {
-				return false;
-			}
+void plusAns(int x,int y){
+	if(arr[x][y] == 0) ans_zero++;
+	else if(arr[x][y] == 1) ans_one++;
+	else ans_mone++;
+}
+
+bool isSame(int x1, int y1, int x2, int y2){
+	int k = arr[x1][y1];
+	for(int i = x1; i < x2; i++){
+		for(int j = y1; j < y2; j++){
+			if(arr[i][j] != k) return false;
 		}
 	}
 	return true;
 }
 
-
-void cut(int a, int b, int n) 
-{
-	if (check(a, b, n)) {
-		ans[arr[a][b] + 1]++;
-		return;
-	}
-
-	int l = n / 3;
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cut(a + i*l, b + j*l, l);
+void func(int x1, int y1, int x2, int y2){
+	if(isSame(x1,y1,x2,y2)) plusAns(x1,y1);
+	else{
+		int gap = x2 - x1;
+		for(int i = x1; i < x2; i+=gap/3){
+			for(int j = y1; j < y2; j+=gap/3){
+				func(i,j,i+gap/3,j+gap/3);
+			}
 		}
 	}
 }
 
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
-	int n; cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	cut(1, 1, n);
-	
-	for (int k = 0; k < 3; k++)
-		cout << ans[k] << '\n';
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	input();
+	func(0,0,n,n);
+	cout << ans_mone << "\n";
+	cout << ans_zero << "\n";
+	cout << ans_one << "\n";
 	return 0;
 }
