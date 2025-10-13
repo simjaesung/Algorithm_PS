99b3b7e709d2e479e39a856f70d771efb73780b1
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-14 00:38:02 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 int n,m;
 int arr[10], memo[10];
 
-void func(int k, int idx){
+void go(int k, int idx){
 	if(k == m){
-		for(int i = 0; i < m; i++){
-			cout << memo[i] << " ";
-		}
+		for(int i = 0; i < m; i++) cout << memo[i] << " ";
 		cout << "\n";
 		return;
 	}
+
 	for(int i = idx; i < n; i++){
 		memo[k] = arr[i];
-		func(k + 1, i);
+		go(k + 1, i);
 	}
 }
 
-
 int main() {
-	char input[100];
 	cin >> n >> m;
 	for(int i = 0; i < n; i++) cin >> arr[i];
 	sort(arr, arr + n);
-	func(0, 0);
+	go(0,0);
 	return 0;
 }
