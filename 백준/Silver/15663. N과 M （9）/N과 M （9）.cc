b89a7fb706621d14498acea6a21624df9383166d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-15 23:34:51 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,39 +1,33 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+int n,m;
+int arr[10], memo[10], isused[10];
 
-int ans[10], arr[10], check[10];
-int n, m;
-
-void go(int k)
-{
-	if (k == m) {
-		for (int i = 0; i < m; i++) cout << ans[i] << ' ';
-		cout << '\n';
+void go(int k){
+	if(k == m){
+		for(int i = 0; i < m; i++) cout << memo[i] << " ";
+		cout << "\n";
 		return;
 	}
 
 	int tmp = 0;
-	for (int i = 0; i < n; i++) {
-		if (check[i]) continue;
-		if (arr[i] == tmp) continue;
-		//이전수와 동일하면
-
-		check[i] = 1; ans[k] = arr[i];
-		tmp = arr[i];
-
-		go(k + 1);
-		check[i] = 0;
+	for(int i = 0; i < n; i++){
+		if(arr[i] != tmp && !isused[i]){
+			memo[k] = arr[i];
+			isused[i] = 1;
+			go(k + 1);
+			tmp = arr[i];
+			isused[i] = 0;
+		}
 	}
+	
 }
 
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
+int main() {
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	sort(arr, arr + n);
-
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr,arr + n);
 	go(0);
 	return 0;
 }
