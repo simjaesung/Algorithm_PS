52389bdcc576a6653ac09e3cfba2d9586fe999a0
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-15 23:41:13 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n,m;
+int arr[10], memo[10];
+
+void go(int k, int idx){
+	if(k == m){
+		for(int i = 0; i < m; i++) cout << memo[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	int tmp = 0;
+	for(int i = idx; i < n; i++){
+		if(arr[i] != tmp){
+			memo[k] = arr[i];
+			go(k + 1, i + 1);
+			tmp = arr[i];
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr,arr + n);
+	go(0, 0);
+	return 0;
+}
