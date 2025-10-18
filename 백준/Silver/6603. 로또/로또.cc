ebc6d11f887f0e7dea97638bc177ef260f04578d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-18 10:16:24 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -1,38 +1,31 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+int n, arr[50], memo[50];
 
-int num[20];
-int check[6];
-int n;
-
-void go(int index, int select)
-{
-	if (select == 6) {
-		for (int i = 0; i < 6; i++) {
-			cout << num[check[i]] << ' ';
-		}
-		cout << '\n';
+void go(int k, int idx){
+	if(k == 6){
+		for(int i = 0; i < 6; i++) cout << memo[i] << " ";
+		cout << "\n";
 		return;
 	}
-	if (index >= n) return;
 
-	check[select] = index;
-	go(index + 1, select + 1);
-	go(index + 1, select);
+	for(int i = idx; i < n; i++){
+		memo[k] = arr[i];
+		go(k + 1, i + 1);
+	}
 }
 
-
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
-	while (1) {
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	while(true){
 		cin >> n;
-		if (n == 0) break;
-
-		for (int i = 0; i < n; i++) cin >> num[i];
-		go(0, 0);
-
-		cout << '\n';
+		if(n == 0) break;
+		for(int i = 0; i < n; i++) cin >> arr[i];
+		go(0,0);
+		cout << "\n";
 	}
 	return 0;
 }
