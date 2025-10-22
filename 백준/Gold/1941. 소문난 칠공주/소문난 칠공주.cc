2005f6512102e3c1aab178f3371146587f4a006d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-22 23:49:52 +0900

    [Gold III] Title: 소문난 칠공주, Time: 80 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -1,75 +1,69 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
+char arr[5][5]; 
+int ans;
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,1,-1};
+vector<pair<int,int>> selected;
 
-char arr[6][6];
-int check[6][6], v[6][6];
-int dx[] = { 1,-1,0,0 };
-int dy[] = { 0,0,1,-1 };
-int p[25];
-
-bool inrange(int x, int y) {
-	return x >= 0 && x < 5 && y >= 0 && y < 5;
+bool inRange(int x, int y){
+	return x >= 0 && x < 5 && y >=0 && y < 5;
 }
 
+bool isConnected(){
+	queue<pair<int,int>> q;
+	bool visited[5][5] = {0};
+	visited[selected[0].first][selected[0].second] = true;
+	q.push(selected[0]);
 
-int som, yeon, cnt, ans = 0;
-
-void dfs(int x, int y)
-{
-	cnt++;
-	if (arr[x][y] == 'S') som++;
-	else yeon++;
-
-	v[x][y] = 1;
-	
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (!inrange(nx, ny)) continue;
-		if(check[nx][ny] == 1 && v[nx][ny] == 0) dfs(nx, ny);
-	}
-}
-
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
-	for (int i = 0; i < 5; i++) {
-		string s; cin >> s;
-		for (int j = 0; j < 5; j++) {
-			arr[i][j] = s[j];
-		}
-	}
-
-	fill(p + 7, p + 25, 1);
-
-	do {
-		//check 배열 초기화
-		for (int i = 0; i < 6; i++)
-			fill(check[i], check[i] + 6, 0);
-		//방문기록 초기화
-		for (int i = 0; i < 6; i++)
-			fill(v[i], v[i] + 6, 0);
-
-		int sx,sy; 
-
-		for (int i = 0; i < 25; i++) {
-			//순열 번호 순대로 정사각형 자리에 배치
-			if (p[i] == 0) {
-				check[i / 5][i % 5] = 1;
-				sx = i / 5;
-				sy = i % 5;
+	int cnt = 1;
+	while(!q.empty()){
+		auto[cx, cy] = q.front(); q.pop();
+		for(int k = 0; k < 4; k++){
+			int nx = cx + dx[k];
+			int ny = cy + dy[k];
+			if(!inRange(nx,ny)) continue;
+			for(auto[sx, sy] : selected){
+				if(sx == nx && sy == ny && !visited[sx][sy]){
+					visited[sx][sy] = true;
+					q.push({sx,sy});
+					cnt++;
+				}
 			}
 		}
-		som = 0; yeon = 0; cnt = 0;
-		dfs(sx, sy);
+	}
+	return cnt == 7;
+}
 
-		//한번의 dfs후 연속 길이가 7, 도연파 4명 이하
-		if (cnt == 7 && yeon < 4) ans++;
+void input(){
+	string s;
+	for(int i = 0; i < 5; i++){
+		cin >> s;
+		for(int j = 0; j < 5; j++) arr[i][j] = s[j];
+	}
+}
 
-	} while (next_permutation(p, p + 25));
+void go(int k, int S, int idx){
+	if(k == 7){
+		if(S >= 4 && isConnected()) ans++;
+		return;
+	}
 
+	for(int i = idx; i < 25; i++){
+		int x = i / 5; int y = i % 5;
+		selected.push_back({x,y});
+		go(k + 1, S + (arr[x][y] == 'S'), i + 1);
+		selected.pop_back();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	input();
+	go(0,0,0);
 	cout << ans;
-
 	return 0;
 }
