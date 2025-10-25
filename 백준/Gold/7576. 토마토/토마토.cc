7d88c1e0544ab135b5562f8cf64b76a1ab97cc13
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-25 11:38:58 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,65 +1,59 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
-
-int box[1005][1005], c[1005][1005] = { 0 };
-int m, n;
-int dx[] = { 0,0,1,-1 };
-int dy[] = { 1,-1,0,0 };
-
-int inrange(int x, int y) {
-	if (x >= 0 && x < n && y >= 0 && y < m) return 1;
-	return 0;
+int n,m,ans;
+int arr[1005][1005];
+int visited[1005][1005];
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
+bool inRange(int x, int y){
+	return x >= 0 && x < n && y >= 0 && y < m;
+}
+void input(){
+	cin >> m >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++) cin >> arr[i][j];
+	}
 }
 
-queue<pair<int, int>>q;
-
-void bfs()
-{
-	while (!q.empty()) {
-		int cx = q.front().first;
-		int cy = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cx + dx[i];
-			int ny = cy + dy[i];
-			if (inrange(nx, ny)) {
-				if (box[nx][ny] == 0) {
-					box[nx][ny] = box[cx][cy] + 1;
-					q.push(make_pair(nx, ny));
-				}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	input();
+	queue<pair<int,int>> q;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(arr[i][j] == 1) {
+				q.push({i,j});
+				visited[i][j] = 1;
 			}
 		}
 	}
-}
 
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> m >> n;
-
-	int check = 0;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> box[i][j];
-			if (box[i][j] == 1) q.push(make_pair(i, j));
+	while(!q.empty()){
+		auto[cx,cy] = q.front(); q.pop();
+		for(int i = 0; i < 4; i++){
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			if(!inRange(nx, ny)) continue;
+			if(arr[nx][ny] == -1) continue;
+			if(visited[nx][ny]) continue;
+			visited[nx][ny] = visited[cx][cy] + 1;
+			q.push({nx,ny});
 		}
 	}
 
-	bfs();
-
-	int ans = -1;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (box[i][j] == 0) {
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(visited[i][j] == 0 && arr[i][j] == 0){
 				cout << -1;
 				return 0;
 			}
-			ans = max(ans, box[i][j]);
+			if(visited[i][j]){
+				ans = max(ans, visited[i][j] - 1);
+			}
 		}
 	}
-	cout << ans - 1;
-
+	cout << ans;
 	return 0;
 }
