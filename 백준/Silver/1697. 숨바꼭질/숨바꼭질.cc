03ff1f69097e74df70806741b3aa8cfd63366a73
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-10-27 22:40:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,39 +1,35 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
-int arr[200005] = { 0 };
 
-void bfs(int x)
-{
-	queue<int>q;
-	q.push(x);
-	arr[x] = 1;
-
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
-		for (int i = 0; i < 3; i++) {
-			int nx;
-			if (i == 0) nx = now + 1;
-			else if (i == 1) nx = now - 1;
-			else nx = now * 2;
-
-			if (nx > 200000 || nx < 0) continue;
-
-			if (arr[nx] == 0) {
-				arr[nx] = arr[now] + 1;
-				q.push(nx);
-			}
-		}
-	}
-}
-
-int main()
-{
-	int n, k;
+int visited[100005];
+int n,k;
+int main() {
 	cin >> n >> k;
-	bfs(n);
-	cout << arr[k] - 1;
+	queue<int> q;
+	q.push(n); 
+	visited[n] = 1;
+	while(!q.empty()){
+		int cx = q.front(); q.pop();
+		if(cx == k){
+			cout << visited[cx] - 1;
+			break;
+		}
+		if(cx - 1 >= 0 && !visited[cx-1]){
+			visited[cx-1] = visited[cx] + 1;
+			q.push(cx-1);
+		}
 
+		if(cx + 1 <= 100000 && !visited[cx+1]){
+			visited[cx+1] = visited[cx] + 1;
+			q.push(cx+1);
+		}
+
+		if(cx * 2 <= 100000 && !visited[cx * 2]){
+			visited[cx * 2] = visited[cx] + 1;
+			q.push(cx * 2);
+		}
+		
+	}
 	return 0;
 }
