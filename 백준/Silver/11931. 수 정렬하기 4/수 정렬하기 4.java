275584c2999a89ab93ce947c1a770bbd110b7790
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-01 11:36:05 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2400 ms, Memory: 195724 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Integer[] arr = new Integer[n];
+		for(int i = 0; i < n; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr, (a,b) -> b - a);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			sb.append(arr[i] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
