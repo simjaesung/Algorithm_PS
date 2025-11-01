7933b9495476efc5ea9de9298eb830911de37034
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-01 12:10:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 740 ms, Memory: 57380 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static class loc{
+		int x;
+		int y;
+		public loc(int x, int y){
+			this.x = x; this.y = y;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		loc[] arr = new loc[n];
+		for(int i = 0; i < n; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[i] = new loc(x,y);
+		}
+
+		Arrays.sort(arr, (a,b) -> {
+			if(a.y == b.y) return a.x - b.x;
+			return a.y - b.y;
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++){
+			sb.append(arr[i].x + " " + arr[i].y + "\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
