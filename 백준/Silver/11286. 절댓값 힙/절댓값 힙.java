4b91c724521e20390b87d5548a4f2f0ef1f344d0
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-03 00:53:34 +0900

    [Silver I] Title: 절댓값 힙, Time: 296 ms, Memory: 26464 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -4,33 +4,20 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		Queue<Integer> pq = new PriorityQueue<>();
-		Map<Integer, Integer> map = new HashMap<>();
 		int n = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < n; i++){
-			int k = Integer.parseInt(br.readLine());
-			
-			if(k == 0){
+
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer>pq = new PriorityQueue<>((a,b) -> {
+			if(Math.abs(a) == Math.abs(b)) return a - b;
+			return Math.abs(a) - Math.abs(b);
+		});
+		while(n-- > 0){
+			int val = Integer.parseInt(br.readLine());
+			if(val == 0){
 				if(pq.isEmpty()) sb.append(0 + "\n");
-				else{
-					int top = pq.poll();
-					if(map.containsKey(-1 * top) && map.get(-1 * top) > 0){
-						sb.append((-1 * top) + "\n");
-						map.put(-1 * top, map.get(-1 * top) - 1);
-					}else{
-						sb.append(top + "\n");
-						map.put(top, map.get(top) - 1);
-					}
-				}
-			} else {
-				map.put(k, map.getOrDefault(k, 0) + 1);
-				pq.add(Math.abs(k));
-			}
+				else sb.append(pq.poll() + "\n");
+			}else pq.offer(val);
 		}
-		
-		
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
 }
