adfb01f7b383950a1b2cfe5c4c32d7d088fcff72
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-06 01:52:38 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18244 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,13 +7,9 @@ class Main {
 		int n = Integer.parseInt(br.readLine());
 		int[] dp = new int[1000005];
 		for(int i = 2; i <= n; i++){
-			if(i % 2 == 0 && i % 3 == 0){
-				dp[i] = Math.min(dp[i/2], Math.min(dp[i/3],dp[i-1])) + 1;
-			}else if(i % 2 == 0){
-				dp[i] = Math.min(dp[i/2],dp[i-1]) + 1;
-			}else if(i % 3 == 0){
-				dp[i] = Math.min(dp[i/3],dp[i-1]) + 1;
-			} else dp[i] = dp[i-1] + 1;
+			dp[i] = dp[i-1] + 1;
+			if(i % 3 == 0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
+			if(i % 2 == 0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
 		}
 		System.out.println(dp[n]);
 	}
