213dca6102b29798d881aa8136df2d60ecc34c94
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-13 23:19:31 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static int INF = Integer.MAX_VALUE / 2;
 	static class Pair{
 		int x; int y;
 		public Pair(int x, int y){
 			this.x = x; this.y = y;
 		}
 	}
+	static int INF = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,61 +17,59 @@ class Main {
 		int m = Integer.parseInt(st.nextToken());
 		int x = Integer.parseInt(st.nextToken());
 
-		ArrayList<Pair>[] adj = new ArrayList[n + 1];
-		for(int i = 1; i <=n; i++) adj[i] = new ArrayList<>();
+		ArrayList<Pair>[] adj1 = new ArrayList[n+1];
+		ArrayList<Pair>[] adj2 = new ArrayList[n+1];
+		for(int i = 1; i <= n; i++){
+			adj1[i] = new ArrayList<>();
+			adj2[i] = new ArrayList<>();
+		}
 		
 		while(m-- > 0){
-			st = new StringTokenizer(br.readLine());			
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int t = Integer.parseInt(st.nextToken());
-			adj[a].add(new Pair(b,t));
+			adj1[a].add(new Pair(b,t));
+			adj2[b].add(new Pair(a,t));
 		}
-		
-		int[] XtoPer = new int[n + 1];
-		Arrays.fill(XtoPer, INF);
-		
-		PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.y - b.y);
+
+		//x에서 각각 노드로 가는 거리
+		int[] xToPer = new int[n+1];
+		Arrays.fill(xToPer, INF);
+		PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.y - b.y);
 		pq.offer(new Pair(x,0));
-		XtoPer[x] = 0;
-		
+		xToPer[x] = 0;
+
 		while(!pq.isEmpty()){
 			Pair cur = pq.poll();
-			if(XtoPer[cur.x] != cur.y) continue;
-
-			for(Pair nxt : adj[cur.x]){
-				if(XtoPer[nxt.x] <= XtoPer[cur.x] + nxt.y) continue;
-				XtoPer[nxt.x] = XtoPer[cur.x] + nxt.y;
-				pq.offer(new Pair(nxt.x, XtoPer[nxt.x]));
+			if(xToPer[cur.x] != cur.y) continue;
+			for(Pair nxt : adj1[cur.x]){
+				if(xToPer[nxt.x] <= nxt.y + xToPer[cur.x]) continue;
+				xToPer[nxt.x] = nxt.y + xToPer[cur.x];
+				pq.offer(new Pair(nxt.x, xToPer[nxt.x]));
 			}
 		}
 
-		//X외 지역에서 X까지의 최단 거리를 구하기 위한
-		int[] perToX = new int[n + 1];
-		for(int i = 1; i <= n; i++) {
-			if(i == x) continue;
-			int[] tmp = new int[n+1]; //임시 배열
-			pq.offer(new Pair(i,0));
-			Arrays.fill(tmp, INF);
-			tmp[i] = 0;
+		//각각 노드에서 x로 가는 거리
+		int[] perToX = new int[n+1];
+		Arrays.fill(perToX, INF);
+		pq.offer(new Pair(x,0));
+		perToX[x] = 0;
 
-			while(!pq.isEmpty()){
-				Pair cur = pq.poll();
-				if(tmp[cur.x] != cur.y) continue;
-	
-				for(Pair nxt : adj[cur.x]){
-					if(tmp[nxt.x] <= tmp[cur.x] + nxt.y) continue;
-					tmp[nxt.x] = tmp[cur.x] + nxt.y;
-					pq.offer(new Pair(nxt.x, tmp[nxt.x]));
-				}
+		while(!pq.isEmpty()){
+			Pair cur = pq.poll();
+			if(perToX[cur.x] != cur.y) continue;
+			for(Pair nxt : adj2[cur.x]){
+				if(perToX[nxt.x] <= nxt.y + perToX[cur.x]) continue;
+				perToX[nxt.x] = nxt.y + perToX[cur.x];
+				pq.offer(new Pair(nxt.x, perToX[nxt.x]));
 			}
-			perToX[i] = tmp[x];
 		}
-		
+
 		int ans = 0;
-		for(int i = 1; i <= n; i++){
-			ans = Math.max(perToX[i] + XtoPer[i], ans);
+		for(int i = 1; i<=n; i++){
+			ans = Math.max(ans, perToX[i] + xToPer[i]);
 		}
-		System.out.print(ans);
+		System.out.println(ans);
 	}
 }
