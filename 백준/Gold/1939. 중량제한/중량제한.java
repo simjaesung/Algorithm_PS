9333abce7f8cb819783546dc2757ea6de5f0ff4c
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-11-15 18:04:38 +0900

    [Gold III] Title: 중량제한, Time: 392 ms, Memory: 48476 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -2,21 +2,29 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static class Pair{
-		int x; int cost;
-		public Pair(int x, int cost){
-			this.x = x; this.cost = cost;
+	static class Tuple{
+		int x; int y; int cost;
+		public Tuple(int x, int y, int cost){
+			this.x = x; this.y = y; this.cost = cost;
 		}
 	}
-	static int INF = Integer.MAX_VALUE / 2;
+	static int ans = Integer.MAX_VALUE / 2;
+	static int[] p;
+
+	static int find(int x){
+		if(p[x] != x) p[x] = find(p[x]);
+		return p[x];
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
+		p = new int[n+1];
+		for(int i = 1; i <= n; i++) p[i] = i;
 
-		ArrayList<Pair>[] adj = new ArrayList[n+1];
-		for(int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
+		PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b) -> b.cost - a.cost);
 		
 		while(m-- > 0){
 			st = new StringTokenizer(br.readLine());
@@ -24,33 +32,23 @@ class Main {
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 
-			adj[a].add(new Pair(b,c));
-			adj[b].add(new Pair(a,c));
+			pq.offer(new Tuple(a,b,c));
 		}
 
 		st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
 
-		//w[i] 는 시작점에서 i까지 갈 때 가져갈 수 있는 물품 최대 중량
-		int[] w = new int[n+1];
-		//시작점 -> 시작점은 무게 제한이 없음
-		w[start] = INF;
-		
-		PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.cost - a.cost);
-		pq.offer(new Pair(start, INF));
-
 		while(!pq.isEmpty()){
-			Pair cur = pq.poll();
-			if(w[cur.x] != cur.cost) continue;
-
-			for(Pair nxt : adj[cur.x]){
-				//다음으로 가려고하는데 이미 더 많이 옮길 수 있을 때 
-				if(w[nxt.x] >= Math.min(w[cur.x],nxt.cost)) continue;
-				w[nxt.x] = Math.min(w[cur.x],nxt.cost);
-				pq.offer(new Pair(nxt.x, w[nxt.x]));
-			}
+			Tuple cur = pq.poll();
+			int px = find(cur.x);
+			int py = find(cur.y);
+			if(px != py) p[py] = px;
+			ans = Math.min(ans, cur.cost);
+			
+			if(find(start) == find(end)) break;
 		}
-		System.out.println(w[end]);
+		
+		System.out.println(ans);
 	}
 }
