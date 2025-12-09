38695743d455ee59717693b26c96d8841ce4e8a9
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-12-10 07:29:37 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -6,19 +6,14 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String a = br.readLine();
 		String b = br.readLine();
-		char[] sortA = a.toCharArray();
-		char[] sortB = b.toCharArray();
-		Arrays.sort(sortA);
-		Arrays.sort(sortB);
 
-		int idxA = 0; int idxB = 0; int same = 0;
-		while(idxA < a.length() && idxB < b.length()){
-			if(sortA[idxA] == sortB[idxB]){
-				same++; idxA++; idxB++;
-			} else if(sortA[idxA] < sortB[idxB]) idxA++;
-			else idxB++; 
-		}
+		int[] cnt = new int[26];
+		for(char c : a.toCharArray()) cnt[c - 'a']++;
+		for(char c : b.toCharArray()) cnt[c - 'a']--;
 
-		System.out.print(a.length() - same + b.length() - same);
+		int ans = 0;
+		for(int k : cnt) ans += Math.abs(k);
+		
+		System.out.print(ans);
 	}
 }
