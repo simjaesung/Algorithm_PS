7ea2656f0773caba2a972a81c1d92f50e8e09b98
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-12-13 19:21:26 +0900

    [Gold III] Title: 줄 세우기, Time: 432 ms, Memory: 45020 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,47 +2,40 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	public static int n,m;
-	public static ArrayList<Integer>[] adj;
-	public static int inCnt[];
-	public static void input() throws Exception{
+	static ArrayList<Integer>[] adj;
+	static int[] rece;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		adj = new ArrayList[n+1];
-		inCnt = new int[n+1];
+		for(int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
+		rece = new int[n+1];
 
-		for(int i = 0; i <= n; i++) adj[i] = new ArrayList<>();
-
-		for(int i = 0; i < m; i++){
+		while(m-- > 0){
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			adj[a].add(b);
-			inCnt[b]++;
+			rece[b]++;
 		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		input();
-		Deque<Integer>dq = new LinkedList<>();
-		for(int i = 1; i <=n; i++) {
-			if(inCnt[i] == 0) dq.addLast(i);
+
+		Deque<Integer> dq = new ArrayDeque<>();
+		for(int i = 1; i <= n; i++) {
+			if(rece[i] == 0) dq.addLast(i);
 		}
 
 		StringBuilder sb = new StringBuilder();
-
 		while(!dq.isEmpty()){
-			int front = dq.removeFirst();
-			sb.append(front + " ");
-			for(int k : adj[front]){
-				inCnt[k]--;
-				if(inCnt[k] == 0) dq.addLast(k);
+			int cur = dq.removeFirst();
+			sb.append(cur + " ");
+			for(int nxt : adj[cur]){
+				rece[nxt]--;
+				if(rece[nxt] == 0) dq.addLast(nxt);
 			}
 		}
 
-		System.out.print(sb.toString());
+		System.out.println(sb);
 	}
 }
