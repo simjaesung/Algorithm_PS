78eee4f9ca8b19aec8a22823724d976615298066
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2025-12-13 21:34:41 +0900

    [Bronze I] Title: 단어 공부, Time: 260 ms, Memory: 23016 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,35 +1,28 @@
 import java.io.*;
-import java.util.*;
-
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		s = s.toUpperCase();
-		char[] charArr = s.toCharArray();
-		int[] arr = new int[26];
-		for(char c : charArr) arr[c-'A']++;
-	
-		int maxCnt = 0;
-		int maxIdx = 0;
-		
+		s = s.toLowerCase();
+
+		int[] cnt = new int[26];
+		for(char c : s.toCharArray()) cnt[c-'a']++;
+
+		int max_cnt = 0;
+		int max_idx = 0;
 		for(int i = 0; i < 26; i++){
-			if(arr[i] > maxCnt){
-				maxCnt = arr[i];
-				maxIdx = i;
+			if(cnt[i] > max_cnt) {
+				max_cnt = cnt[i];
+				max_idx = i;
 			}
 		}
-		
-		if(!checkMax(arr,maxCnt,maxIdx)) System.out.println("?");
-		else System.out.println((char)(maxIdx + 'A'));
-	}
-	
-	public static boolean checkMax(int[] arr, int maxCnt, int maxIdx){
+
+		int same_ans = 0;
 		for(int i = 0; i < 26; i++){
-			if(arr[i] == maxCnt && i != maxIdx ){
-				return false;
-			}
+			if(cnt[i] == max_cnt) same_ans++;
 		}
-		return true;
+
+		if(same_ans > 1) System.out.println("?");
+		else System.out.println((char)(max_idx + 'A'));
 	}
 }
