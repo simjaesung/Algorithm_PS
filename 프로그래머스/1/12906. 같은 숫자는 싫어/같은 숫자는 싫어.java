9c932c34b50e7d3f4386f9f2a568c7ca03e63f00
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-01-21 07:55:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.60 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,20 +1,14 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public Deque<Integer> solution(int []arr) {
         Deque<Integer> dq = new ArrayDeque<>();
         for(int val : arr){
             if(dq.isEmpty() || dq.getLast() != val){
                 dq.addLast(val);
             }
         }
-        
-        int[] answer = new int[dq.size()];
-        int i = 0;
-        while(!dq.isEmpty()){
-            answer[i++] = dq.removeFirst();
-        }
 
-        return answer;
+        return dq;
     }
 }
