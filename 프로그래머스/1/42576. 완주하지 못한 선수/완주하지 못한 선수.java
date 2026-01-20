8b2616efd60ab6a9eb6211089ddfee30155e35b3
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-01-21 08:42:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.82 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public Map<String,Integer> map = new HashMap<>();
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
-        for(String s : completion) map.put(s, map.getOrDefault(s, 0) + 1); 
-        
-        for(String s : participant) {
-            if(!map.containsKey(s) || map.get(s) == 0) {
-                answer = s;
-                break;
-            }
-            map.put(s, map.get(s) - 1); 
+        Map<String, Integer> m = new HashMap<>();
+        for(String c : completion){
+            m.put(c, m.getOrDefault(c, 0) + 1);
         }
+        
+        for(String p: participant){
+            if(!m.containsKey(p) || m.get(p) == 0){
+                answer = p;
+                break;
+            } else m.put(p, m.get(p) - 1);
+        }
+    
         return answer;
     }
 }
