97ac416bd0b264d6f33715bc0febb024a0fce019
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-01-27 07:43:59 +0900

    [level 1] Title: K번째수, Time: 0.44 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,17 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        int idx = 0;
+    public List<Integer> solution(int[] array, int[][] commands) {
+        List<Integer> answer = new ArrayList<>();
         for(int[] command : commands){
             int i = command[0];
             int j = command[1];
             int k = command[2];
-            
-            ArrayList<Integer> tmp = new ArrayList<>();
-            for(int s = i - 1; s < j; s++) tmp.add(array[s]);
-            Collections.sort(tmp);
-            answer[idx++] = tmp.get(k-1);
+            int[] sliced = Arrays.copyOfRange(array,i-1,j);
+            Arrays.sort(sliced);
+            answer.add(sliced[k-1]);
         }
+        
         return answer;
     }
 }
