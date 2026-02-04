432e07b36b7989e1fcfb0b5b5b25bb3d93a4f36a
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-02-05 01:27:50 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,27 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        TreeSet<Integer> remains = new TreeSet<>();
-        for(int r : reserve) remains.add(r);
-        
-        int[] arr = new int[n+2];
-        Arrays.fill(arr, 1);  // 전체 1로 초기화 (간단!)
+        Set<Integer> reserves = new HashSet<>();
+        Set<Integer> losts = new HashSet<>();
+        for(int r : reserve) reserves.add(r);
         
         for(int l : lost){
-            if(remains.contains(l)){
-                remains.remove(l);  // 여벌 있으면 빌려줄 목록 제거 (본인 자동사용)
-            } else {
-                arr[l] = 0;         // 여벌 없으면만 도난 처리
-            }
+            if(reserves.contains(l)) reserves.remove(l);
+            else losts.add(l);
         }
         
-        for(int r : remains){
-            if(arr[r-1] == 0) arr[r-1] = 1;
-            else if(arr[r+1] == 0) arr[r+1] = 1;
+        for(int r : reserves){
+            if(losts.contains(r-1)) losts.remove(r-1);
+            else if(losts.contains(r+1)) losts.remove(r+1);
         }
         
-        int answer = 0;
-        for(int i = 1; i <= n; i++) if(arr[i] == 1) answer++;
-        return answer;
+        return n-losts.size();
     }
 }
