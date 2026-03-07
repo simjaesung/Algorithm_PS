09304c02beb88d913285f7f12f792ded5ae3413d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-07 11:40:11 +0900

    [level 2] Title: 프로세스, Time: 2.04 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -11,23 +11,22 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int n = priorities.length;
         Deque<Pair> dq = new ArrayDeque<>();
-        Deque<Integer> pri = new ArrayDeque<>();
-        
-        for(int i = 0; i < n; i++) dq.add(new Pair(priorities[i], i));
-        Arrays.sort(priorities);
-        for(int i = n-1; i >=0; i--) pri.add(priorities[i]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        for(int i = 0; i < n; i++){
+            dq.addLast(new Pair(priorities[i], i));
+            pq.add(priorities[i]);
+        }
         
         int answer = 0;
         while(!dq.isEmpty()){
             Pair cur = dq.removeFirst();
-            if(cur.val == pri.getFirst()){
+            if(cur.val == pq.peek()){
+                pq.poll();
                 answer++;
                 if(cur.idx == location) break;
-                pri.removeFirst();
             }
             dq.addLast(cur);
         }
-        
         return answer;
     }
 }
