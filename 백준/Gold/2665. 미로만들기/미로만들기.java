8dfbc481213811f9718c584932310ab5df4819e9
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-11 08:30:50 +0900

    [Gold IV] Title: 미로만들기, Time: 116 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static class Pair{
-		int x; int y;
-		public Pair(int x, int y){
-			this.x = x; this.y = y;
+	static class Tuple{
+		int x; int y; int cnt;
+		public Tuple(int x, int y, int cnt){
+			this.x = x; this.y = y; this.cnt = cnt;
 		}
 	}
 	
@@ -27,12 +27,11 @@ class Main {
 
 		cost[0][0] = 0; 
 
-		Deque<Pair> dq = new ArrayDeque<>();
-		dq.addLast(new Pair(0,0));
+		PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b) -> a.cnt - b.cnt);
+		pq.offer(new Tuple(0,0,0));
 		
-		
-		while(!dq.isEmpty()){
-			Pair cur = dq.removeFirst();
+		while(!pq.isEmpty()){
+			Tuple cur = pq.poll();
 			if(cur.x == n-1 && cur.y == n-1) continue;
 			
 			for(int i = 0; i < 4; i++){
@@ -45,8 +44,7 @@ class Main {
 				if(cost[nx][ny] <= nxtCost) continue;
 				cost[nx][ny] = nxtCost;
 
-				if(arr[nx][ny] == 0) dq.addLast(new Pair(nx,ny));
-				else dq.addFirst(new Pair(nx,ny));
+				pq.offer(new Tuple(nx,ny,nxtCost));
 			}
 		}
 
