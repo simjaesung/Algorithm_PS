c8c5e5dddd333ea6b8f057418359adb90dce7976
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-11 19:13:14 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 109116 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,42 +4,42 @@ import java.util.*;
 class Main {
 	static class Pair{
 		int x; //정점
-		int y; //거리
+		int y; //간선
 		public Pair(int x, int y){
 			this.x = x; this.y = y;
 		}
 	}
-	static int[] dist;
-	static List<Pair>[] adj;
+
+	static int INF = Integer.MAX_VALUE;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 
-		dist = new int[V+1];
-		for(int i = 0; i <= V; i++) dist[i] = Integer.MAX_VALUE;
-		
-		adj = new ArrayList[V+1];
+		int[] dist = new int[V + 1];
+		Arrays.fill(dist, INF);
+		ArrayList<Pair>[] adj = new ArrayList[V + 1];
 		for(int i = 0; i <= V; i++) adj[i] = new ArrayList<>();
+		
+		int start = Integer.parseInt(br.readLine());
 
-		int k = Integer.parseInt(br.readLine());
-
-		while(E-- >0){
+		while(E-- > 0){
 			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			adj[u].add(new Pair(v,w));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			adj[a].add(new Pair(b,cost));
 		}
 
-		PriorityQueue<Pair>pq = new PriorityQueue<>((a,b) -> a.y - b.y);
-		pq.offer(new Pair(k,0));
-		dist[k] = 0;
+		PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.y - b.y);
+		pq.offer(new Pair(start, 0));
+		dist[start] = 0;
 
 		while(!pq.isEmpty()){
-			Pair cur = pq.peek(); pq.poll();
-			if(cur.y != dist[cur.x]) continue;
+			Pair cur = pq.poll();
+			if(dist[cur.x] != cur.y) continue;
 			for(Pair nxt : adj[cur.x]){
 				if(dist[nxt.x] <= dist[cur.x] + nxt.y) continue;
 				dist[nxt.x] = dist[cur.x] + nxt.y;
@@ -47,11 +47,10 @@ class Main {
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= V; i++) {
-			if(dist[i] == Integer.MAX_VALUE) sb.append("INF\n");
+		for(int i = 1; i <= V; i++){
+			if(dist[i] == INF) sb.append("INF\n");
 			else sb.append(dist[i] + "\n");
 		}
-
 		System.out.println(sb);
 	}
 }
