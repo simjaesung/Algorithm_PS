d0b58653018a2ded5de346b683fed59cc48e828c
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-11 18:51:38 +0900

    [Gold IV] Title: 미로만들기, Time: 112 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -8,48 +8,43 @@ class Main {
 			this.x = x; this.y = y;
 		}
 	}
-	
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
+	static int n;
+	static int[][] arr, visited;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[][] arr = new int[n][n];
-		int[][] cost = new int[n][n];
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n][n];
+		visited = new int[n][n];
 		
 		for(int i = 0; i < n; i++){
-			String s = br.readLine();
-			char[] sToChar = s.toCharArray();
-			for(int j = 0; j < n; j++) arr[i][j] = sToChar[j] - '0';
-			Arrays.fill(cost[i],n*n);
+			String input = br.readLine();
+			char[] tmp = input.toCharArray();
+			for(int j = 0; j < n; j++) arr[i][j] = tmp[j] - '0';
+			Arrays.fill(visited[i], n * n);
 		}
 
-		cost[0][0] = 0; 
-
-		Deque<Pair> dq = new ArrayDeque<>();
+		Deque<Pair>dq = new ArrayDeque<>();
+		visited[0][0] = 0;
 		dq.addLast(new Pair(0,0));
-		
-		
 		while(!dq.isEmpty()){
 			Pair cur = dq.removeFirst();
-			if(cur.x == n-1 && cur.y == n-1) continue;
-			
-			for(int i = 0; i < 4; i++){
-				int nx = dx[i] + cur.x;
-				int ny = dy[i] + cur.y;
-				if(nx >= n || ny >= n || nx < 0 || ny < 0) continue;
 
-				int nxtCost = cost[cur.x][cur.y];
-				if(arr[nx][ny] == 0) nxtCost++;
-				if(cost[nx][ny] <= nxtCost) continue;
-				cost[nx][ny] = nxtCost;
+			for(int i = 0; i < 4; i++){
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+				int nxtBroken = visited[cur.x][cur.y];
+				if(arr[nx][ny] == 0) nxtBroken++;
+				if(visited[nx][ny] <= nxtBroken) continue;
+				visited[nx][ny] = nxtBroken;
 				dq.addLast(new Pair(nx,ny));
 			}
 		}
 
-		int ans = cost[n-1][n-1];
-		if(ans == n * n) ans = 0;
-		System.out.println(ans);
+		if(visited[n-1][n-1] == n * n) System.out.print(0);
+		else System.out.print(visited[n-1][n-1]);
 	}
 }
