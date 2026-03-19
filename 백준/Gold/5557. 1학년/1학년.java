ed9cbef7fb58c1ae268d6f8c14b545a1707ddbc8
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-19 21:59:18 +0900

    [Gold V] Title: 1학년, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -5,22 +5,19 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[n + 1];
-		for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+		//dp[i][j] : i번째수까지 사용했을 때 j가 나오는 경우의 수
 		long[][] dp = new long[n][21];
-		dp[1][arr[1]] = 1;
-		
-		for(int i = 2; i < n; i++){
+		dp[0][arr[0]] = 1;
+		for(int i = 1; i < n; i++){
 			for(int j = 0; j <= 20; j++){
-				if(dp[i-1][j] > 0){
-					if(j + arr[i] <= 20) dp[i][j + arr[i]] += dp[i-1][j];
-					if(j - arr[i] >= 0) dp[i][j - arr[i]] += dp[i-1][j];
-				}
+				if(j-arr[i] >= 0) dp[i][j] += dp[i-1][j-arr[i]];
+				if(j+arr[i] <= 20) dp[i][j] += dp[i-1][j+arr[i]];
 			}
 		}
-
-		System.out.println(dp[n-1][arr[n]]);
+		System.out.println(dp[n-2][arr[n-1]]);
 	}
 }
