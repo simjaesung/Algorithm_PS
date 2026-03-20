38ced7e4844556e317d62619fef1c2efed47b5ca
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-21 01:34:15 +0900

    [Gold V] Title: 집합의 표현, Time: 600 ms, Memory: 56840 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,11 +3,10 @@ import java.util.*;
 
 class Main {
 	static int[] p;
-	public static int find(int x){
-		if(x != p[x]){
-			return p[x] = find(p[x]);
-		}
-		return p[x];
+
+	static int find(int x){
+		if(p[x] != x) return p[x] = find(p[x]);
+		return x;
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -15,25 +14,26 @@ class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
+
 		p = new int[n+1];
-		for(int i = 1; i <= n; i++) p[i] = i;
-		
+		for(int i = 0; i <= n; i++) p[i] = i;
+
 		StringBuilder sb = new StringBuilder();
+		
 		while(m-- > 0){
 			st = new StringTokenizer(br.readLine());
-			int mode = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if(mode == 0){
-				int p1 = find(a);
-				int p2 = find(b);
-				p[p1] = p2;
+			if(k == 0) {
+				int pa = find(a);
+				int pb = find(b);
+				p[pa] = pb;
 			}else{
 				if(find(a) == find(b)) sb.append("YES\n");
 				else sb.append("NO\n");
 			}
 		}
-		
 		System.out.println(sb);
 	}
 }
