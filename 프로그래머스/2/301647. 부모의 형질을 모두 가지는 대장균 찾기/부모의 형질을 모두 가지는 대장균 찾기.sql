796cb9e7912e5a2ab2b64431ca77e20776c50ab9
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-22 22:13:50 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select 
+    C.ID, C.GENOTYPE, P.GENOTYPE PARENT_GENOTYPE
+from ECOLI_DATA C
+join ECOLI_DATA P
+on C.PARENT_ID = P.ID
+where P.GENOTYPE & C.GENOTYPE = P.GENOTYPE
+order by C.ID;
