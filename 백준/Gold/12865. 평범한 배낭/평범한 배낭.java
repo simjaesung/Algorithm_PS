4f0e82002713125ef3b916764195b2ef016f642a
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-23 23:33:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53792 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,33 +1,29 @@
 import java.io.*;
 import java.util.*;
-
 class Main {
-	static class Pair{
-		int w; int v;
-		public Pair(int w, int v){
-			this.w = w; this.v = v;
-		}
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		Pair[] arr = new Pair[n+1];
+		int[][] arr = new int[n+1][2];
 		for(int i = 1; i <= n; i++){
 			st = new StringTokenizer(br.readLine());
-			arr[i] = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			arr[i][0] = w; 
+			arr[i][1] = v;
 		}
 
+		//dp[i][j] = i번째 물건을 넣으려고할때 무게가 j이상일때 최대 가치
 		int[][] dp = new int[n+1][k+1];
 		for(int i = 1; i <= n; i++){
 			for(int j = 0; j <= k; j++){
-				if(arr[i].w > j) dp[i][j] = dp[i-1][j];
-				else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - arr[i].w] + arr[i].v);
+				if(arr[i][0] <= j) dp[i][j] = Math.max(dp[i-1][j-arr[i][0]] + arr[i][1], dp[i-1][j]);
+				else dp[i][j] = dp[i-1][j];
 			}
 		}
-		System.out.print(dp[n][k]);
+		System.out.println(dp[n][k]);
 	}
 }
