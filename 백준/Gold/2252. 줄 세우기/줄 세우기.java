beb2da7375ad3b48f4a770f296282cec11968e0d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-27 02:25:52 +0900

    [Gold III] Title: 줄 세우기, Time: 468 ms, Memory: 46136 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,17 +1,15 @@
 import java.io.*;
 import java.util.*;
-
 class Main {
-	static ArrayList<Integer>[] adj;
-	static int[] rece;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		adj = new ArrayList[n+1];
+
+		ArrayList<Integer>[] adj = new ArrayList[n+1];
 		for(int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
-		rece = new int[n+1];
+		int[] rece = new int[n+1];
 
 		while(m-- > 0){
 			st = new StringTokenizer(br.readLine());
@@ -22,20 +20,16 @@ class Main {
 		}
 
 		Deque<Integer> dq = new ArrayDeque<>();
-		for(int i = 1; i <= n; i++) {
-			if(rece[i] == 0) dq.addLast(i);
-		}
-
+		for(int i = 1; i <= n; i++) if(rece[i] == 0) dq.addLast(i);
 		StringBuilder sb = new StringBuilder();
 		while(!dq.isEmpty()){
 			int cur = dq.removeFirst();
 			sb.append(cur + " ");
-			for(int nxt : adj[cur]){
+			for(int nxt : adj[cur]) {
 				rece[nxt]--;
 				if(rece[nxt] == 0) dq.addLast(nxt);
 			}
 		}
-
 		System.out.println(sb);
 	}
 }
