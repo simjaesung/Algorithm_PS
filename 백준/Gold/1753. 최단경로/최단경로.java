d27b484793f305da34b7505ca55258301aaa11fc
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-27 01:46:18 +0900

    [Gold IV] Title: 최단경로, Time: 684 ms, Memory: 109936 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,21 +2,21 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static int INF = Integer.MAX_VALUE;
 	static class Pair{
-		int x; //간선
-		int y; //가중치
-		public Pair(int x, int y){
-			this.x = x; this.y = y;
+		int v; //간선
+		int w; //가중치
+		public Pair(int v, int w){
+			this.v = v; this.w = w;
 		}
 	}
+	static int INF = Integer.MAX_VALUE;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(br.readLine());
 
+		int start = Integer.parseInt(br.readLine());
 		ArrayList<Pair>[] adj = new ArrayList[V+1];
 		for(int i = 0; i <= V; i++) adj[i] = new ArrayList<>();
 		while(E-- > 0){
@@ -27,26 +27,25 @@ class Main {
 			adj[u].add(new Pair(v,w));
 		}
 
-		PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.y - b.y);
-
+		PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.w - b.w);
+		pq.offer(new Pair(start, 0));
 		int[] dist = new int[V+1];
-		Arrays.fill(dist, INF);
+		for(int i = 0; i <= V; i++) dist[i] = INF;
 		dist[start] = 0;
 
-		pq.offer(new Pair(start, 0));
 		while(!pq.isEmpty()){
 			Pair cur = pq.poll();
-			if(dist[cur.x] != cur.y) continue;
+			if(dist[cur.v] != cur.w) continue;
 
-			for(Pair nxt : adj[cur.x]){
-				if(dist[cur.x] + nxt.y >= dist[nxt.x]) continue;
-				dist[nxt.x] = dist[cur.x] + nxt.y;
-				pq.offer(new Pair(nxt.x, dist[nxt.x]));
+			for(Pair nxt : adj[cur.v]){
+				if(dist[cur.v] + nxt.w >= dist[nxt.v]) continue;
+				dist[nxt.v] = dist[cur.v] + nxt.w;
+				pq.offer(new Pair(nxt.v, dist[nxt.v]));
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= V; i++){
+		for(int i = 1; i <= V; i++) {
 			if(dist[i] == INF) sb.append("INF\n");
 			else sb.append(dist[i] + "\n");
 		}
