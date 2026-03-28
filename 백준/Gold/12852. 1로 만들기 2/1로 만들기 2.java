ba78516e6d807b68b2a3b8c8f6ab761cf99e99eb
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-03-29 01:57:49 +0900

    [Gold V] Title: 1로 만들기 2, Time: 188 ms, Memory: 43228 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,38 +1,33 @@
 import java.io.*;
-import java.util.*;
-
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n+1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[1] = 0;
-		int[] pre = new int[n+1];
-		
+		int[][] dp = new int[n+1][2];
 		for(int i = 2; i <= n; i++){
-			if(i % 3 == 0 && dp[i] > dp[i / 3] + 1){
-				dp[i] = dp[i/3] + 1;
-				pre[i] = i/3;
+			dp[i][0] = dp[i-1][0] + 1; //횟수
+			dp[i][1] = i - 1; //이전 idx
+			if(i % 3 == 0){
+				if(dp[i][0] > dp[i/3][0] + 1){
+					dp[i][0] = dp[i/3][0] + 1;
+					dp[i][1] = i/3;
+				}
 			}
-			if(i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
-				dp[i] = dp[i/2] + 1;
-				pre[i] = i/2;
-			}
-			if(dp[i] > dp[i - 1] + 1){
-				dp[i] = dp[i-1] + 1;
-				pre[i] = i-1;
+			if(i % 2 == 0){
+				if(dp[i][0] > dp[i/2][0] + 1){
+					dp[i][0] = dp[i/2][0] + 1;
+					dp[i][1] = i/2;
+				}
 			}
 		}
-		
-		System.out.println(dp[n]);
-		int cur = n;
+
 		StringBuilder sb = new StringBuilder();
-		while(cur != 1){
-			sb.append(cur + " ");
-			cur = pre[cur];
+		int val = n;
+		while(val > 0){
+			sb.append(val + " ");
+			val = dp[val][1];
 		}
-		sb.append(cur);
-		System.out.print(sb);
+		System.out.println(dp[n][0]);
+		System.out.println(sb);
 	}
 }
