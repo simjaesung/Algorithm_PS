ef628f798bb3f32e99f6b3b5f49d3be7dc8f508d
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-01 01:33:52 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 24060 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -10,25 +10,17 @@ class Main {
 		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-		int p1 = 0, p2 = 0, sum = 0, ans = n+1;
+		int left = 0, right = 0, sum = 0, ans = n+1;
 		
-		while(p2 < n){
+		while(left < n){
 			if(sum < s){
-				sum += arr[p2++];
+				if(right == n) break;
+				sum += arr[right++];
 			}else{
-				ans = Math.min(ans, p2 - p1);
-				sum -= arr[p1++];
+				ans = Math.min(ans, right - left);
+				sum -= arr[left++];
 			}
 		}
-		
-		while(p1 < n){
-			if(sum >= s){
-				ans = Math.min(ans, p2 - p1);
-			}
-			sum -= arr[p1++];
-		}
-		
-		if(ans == n+1) System.out.println(0);
-		else System.out.println(ans);
+		System.out.println(ans == n+1? 0 : ans);
 	}
 }
