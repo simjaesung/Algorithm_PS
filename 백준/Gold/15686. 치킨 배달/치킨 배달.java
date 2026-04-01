36df268ed7425dd2a7246bc47e02f690a9675971
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-02 08:18:27 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 17268 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int n,m,ans = Integer.MAX_VALUE;
+	static boolean[] used;
+	static List<Pair> chicken = new ArrayList<>();
+	static List<Pair> house = new ArrayList<>();
+
+	static class Pair{
+		int x; int y;
+		public Pair(int x, int y){
+			this.x = x; this.y = y;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		for(int i = 0; i < n; i++){
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++){
+				int val = Integer.parseInt(st.nextToken());
+				if(val == 1) house.add(new Pair(i,j));
+				if(val == 2) chicken.add(new Pair(i,j));
+			}
+		}
+		used = new boolean[chicken.size()];
+		go(0,0);
+		System.out.println(ans);
+	}
+
+	static void go(int k, int idx){
+		if(k > 0 && k <= m){
+			int tmp = calChickenLoad();
+			ans = Math.min(ans,tmp);
+		}
+		if(k == m) return;
+		
+		for(int i = idx; i < chicken.size(); i++){
+			if(used[i]) continue;
+			used[i] = true;
+			go(k + 1, i + 1);
+			used[i] = false;
+		}
+	}
+
+	static int calChickenLoad(){
+		int tmp = 0;
+		for(int i = 0; i < house.size(); i++){
+			int chickenLoad = 101;
+			int x = house.get(i).x;
+			int y = house.get(i).y;
+			for(int j = 0; j < chicken.size(); j++){
+				if(!used[j]) continue;
+				int len = Math.abs(chicken.get(j).x - x) + Math.abs(chicken.get(j).y - y);
+				chickenLoad = Math.min(chickenLoad, len);
+			}
+			tmp += chickenLoad;
+		}
+		return tmp;
+	}
+}
