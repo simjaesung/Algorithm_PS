2bdee66fbf9ea1ffeee6b73e4afdab27ff6e27be
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-05 01:25:51 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 16696 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,12 +1,6 @@
 import java.io.*;
 import java.util.*;
 class Main {
-	static class Pair{
-		int x; int y;
-		public Pair(int x, int y){
-			this.x = x; this.y = y;
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,23 +10,23 @@ class Main {
 		int INF = Integer.MAX_VALUE;
 		Arrays.fill(visited, INF);
 		
-		Deque<Pair> q = new ArrayDeque<>();
-		q.addLast(new Pair(n,0));
+		Deque<Integer> q = new ArrayDeque<>();
+		q.addLast(n);
 		visited[n] = 0;
 		while(!q.isEmpty()){
-			Pair cur = q.removeFirst();
-			int loc = cur.x; int cnt = cur.y;
+			int loc = q.removeFirst();
+			int cnt = visited[loc];
 			if(loc * 2 <= 100000 && cnt < visited[loc*2]){
 				visited[loc*2] = cnt;
-				q.addLast(new Pair(loc*2,cnt));
+				q.addFirst(loc*2);
 			}
 			if(loc + 1 <= 100000 && cnt + 1 < visited[loc+1]){
 				visited[loc+1] = cnt+1;
-				q.addLast(new Pair(loc+1,cnt+1));
+				q.addLast(loc+1);
 			}
 			if(loc - 1 >= 0 && cnt + 1 < visited[loc-1]){
 				visited[loc-1] = cnt+1;
-				q.addLast(new Pair(loc-1,cnt+1));
+				q.addLast(loc-1);
 			}
 		}
 		System.out.println(visited[k]);
