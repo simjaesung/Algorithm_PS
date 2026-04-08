78253afb6b1f10183bceb1cace2d1b98e5f65025
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-08 13:47:15 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 268 ms, Memory: 22244 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -2,54 +2,47 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static class Pair{
-		int nagu; boolean isRobot;
-		public Pair(int nagu, boolean isRobot){
-			this.nagu = nagu; this.isRobot = isRobot;
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); //컨베이어벨트 2*n
-		int k = Integer.parseInt(st.nextToken()); //내구도 종료 조건
 
-		Deque<Pair> dq = new ArrayDeque<>();
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < 2*n; i++) {
-			dq.addLast(new Pair(Integer.parseInt(st.nextToken()),false));
-		}
+		int[] belt = new int[n*2];
+		for(int i = 0; i < n*2; i++) belt[i] = Integer.parseInt(st.nextToken());
 
-		int phase = 1;
+		boolean[] robot = new boolean[n];
+
+		int phase = 0;
+		int start = 0;
 		while(true){
-			//1. 컨베이어 벨트 이동
-			dq.addFirst(dq.removeLast());
-			List<Pair> belt = new ArrayList<>(dq);
-			//내리는위치 버리기
-			if(belt.get(n-1).isRobot) belt.set(n-1, new Pair(belt.get(n-1).nagu, false));
+			phase++;
+			start = (start - 1 + 2*n) % (2*n);
+			for(int i = n-1; i > 0; i--) robot[i] = robot[i-1];
+			robot[n-1] = false;
+			robot[0] = false;
 
-			//2. 로봇 이동
-			for(int i = n-2; i>=0; i--){
-				if(!belt.get(i).isRobot) continue;
-				if(!belt.get(i+1).isRobot && belt.get(i+1).nagu > 0){
-					belt.set(i+1, new Pair(belt.get(i+1).nagu - 1, true));
-					belt.set(i, new Pair(belt.get(i).nagu, false));
+			for(int i = n-1; i > 0; i--){
+				if(!robot[i] && robot[i-1] && belt[(start + i) % (2*n)] > 0){
+					robot[i-1] = false;
+					robot[i] = true;
+					belt[(start + i) % (2*n)]--;
 				}
 			}
-            //내리는위치 버리기
-			if(belt.get(n-1).isRobot) belt.set(n-1, new Pair(belt.get(n-1).nagu, false));
+			robot[n-1] = false;
 
-			//3. 올리는 위치에 있는 칸의 내구도가 0이 아니면
-			if(!belt.get(0).isRobot && belt.get(0).nagu > 0){
-				belt.set(0, new Pair(belt.get(0).nagu-1, true));
+			if(belt[start] > 0){
+				belt[start]--;
+				robot[0] = true;
 			}
-
+			
 			int cnt = 0;
-			for(Pair b : belt) if(b.nagu == 0) cnt++;
+			for(int nagu : belt) if(nagu == 0) cnt++;
 			if(cnt >= k) break;
-			dq = new ArrayDeque<>(belt);
-			phase++;
 		}
+		
 		System.out.println(phase);
 	}
 }
