0a02ef30d521a0793effbeda14a41646d4d7ed80
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-11 12:20:02 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -2,20 +2,19 @@
 SELECT
     H.HISTORY_ID,
     (
-        ROUND(
-            (DATEDIFF(H.END_DATE, H.START_DATE) + 1) *
-            C.DAILY_FEE * (100 - COALESCE(P.DISCOUNT_RATE,0)) / 100
-            ,0)
-    ) FEE
+        (DATEDIFF(H.END_DATE, H.START_DATE) + 1) *
+        C.DAILY_FEE * (100 - IFNULL(P.DISCOUNT_RATE,0)) / 100) FEE
 from CAR_RENTAL_COMPANY_CAR C
-join CAR_RENTAL_COMPANY_RENTAL_HISTORY H on C.CAR_ID = H.CAR_ID
-left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P on C.CAR_TYPE = P.CAR_TYPE
-and(
-    case 
-        when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 90 then P.DURATION_TYPE = '90일 이상'
-        when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 30 then P.DURATION_TYPE = '30일 이상'
-        when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 7 then P.DURATION_TYPE = '7일 이상'
-    end
-)
+    join CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+        on C.CAR_ID = H.CAR_ID
+    left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P
+        on C.CAR_TYPE = P.CAR_TYPE
+        and(
+            case 
+                when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 90 then P.DURATION_TYPE = '90일 이상'
+                when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 30 then P.DURATION_TYPE = '30일 이상'
+                when DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 7 then P.DURATION_TYPE = '7일 이상'
+            end
+        )
 where C.CAR_TYPE = '트럭'
 order by FEE desc, H.HISTORY_ID desc;
