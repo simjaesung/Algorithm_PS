d85aecc1cb50038550a3688189b56f35d028f1f8
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-17 19:08:52 +0900

    [level 2] Title: 카펫, Time: 7.56 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,17 +2,15 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int col = 1; col * col <= yellow; col++){
-            if(yellow % col != 0) continue;
-            int row = yellow / col;
-            int around = (col + row) * 2;
-            if(brown - around == 4) {
-                answer[0] = row + 2;
-                answer[1] = col + 2;
+        for(int yLow = yellow; yLow >= 1; yLow--){
+            if(yellow % yLow != 0) continue;
+            int yCol = yellow / yLow;
+            if(yLow * 2 + yCol * 2 + 4 == brown){
+                answer[0] = yLow + 2;
+                answer[1] = yCol + 2;
                 break;
             }
         }
-        
         return answer;
     }
 }
