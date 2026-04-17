77483994318070077554b93f1b42a55bc2250cd0
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-17 21:28:03 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 348 ms, Memory: 29012 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,104 +1,133 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-	static int[][] arr, arr_copy;
-	static int R,C,T,H1,H2,ans;
-	static int dx[] = {-1,1,0,0};
-	static int dy[] = {0,0,-1,1};
+	static int r,c,t,x1,x2;
+	static int[][] arr;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
-		arr = new int[R][C];
-		for(int i = 0; i < R; i++){
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		t = Integer.parseInt(st.nextToken());
+		arr = new int[r+1][c+1];
+		for(int i = 1; i <= r; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < C; j++) {
+			for(int j = 1; j <= c; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(arr[i][j] == -1){
+					if(x1 == 0) x1 = i;
+					else x2 = i;
+				}
+			}
+		}
+
+		while(t-- > 0){
+			int[][] copyArr = new int[r+1][c+1];
+		
+			//확산
+			spread(copyArr);
+
+			//위쪽 순환(반시계)
+			rotateTop(copyArr);
+
+			//아래쪽 순환(시계)
+			rotateBottom(copyArr);
+
+			copyArr[x1][1] = -1;
+			copyArr[x2][1] = -1;
+
+			//복사
+			for(int i = 1; i <= r; i++){
+				for(int j = 1; j <= c; j++) {
+					arr[i][j] = copyArr[i][j];
+				}
+			}
+		}
+
+		int ans = 0;
+		for(int i = 1; i <= r; i++){
+			for(int j = 1; j <= c; j++) {
+				if(arr[i][j] != -1) ans += arr[i][j];
 			}
 		}
 		
-		for(int i = 0; i < R; i++){
-			if(arr[i][0] == -1){
-				H1 = i;
-				H2 = i+1;
-				break;
-			}
-		}
-		
-		while(T-- > 0){
-			phase1();
-			phase2();
-			copy();
-		}
-		cntAns();
 		System.out.println(ans);
 	}
 
-	//먼지 확산
-	static void phase1(){
-		arr_copy = new int[R][C];
-		for(int i = 0; i < R; i++){
-			for(int j = 0; j < C; j++) {
+	static void spread(int[][] copyArr){
+		for(int i = 1; i <= r; i++){
+			for(int j = 1; j <= c; j++) {
 				if(arr[i][j] == 0 || arr[i][j] == -1) continue;
-				int tmp = arr[i][j] / 5;
 				int cnt = 0;
 				for(int k = 0; k < 4; k++){
 					int nx = i + dx[k];
 					int ny = j + dy[k];
-					if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+					if(nx <= 0 || nx > r || ny <= 0 || ny > c) continue;
 					if(arr[nx][ny] == -1) continue;
-					arr_copy[nx][ny] += tmp;
+					copyArr[nx][ny] += arr[i][j] / 5;
 					cnt++;
 				}
-				arr_copy[i][j] += arr[i][j] - (tmp * cnt);
+				copyArr[i][j] += arr[i][j] - (arr[i][j] / 5) * cnt;
 			}
 		}
 	}
 
-	//이동
-	static void phase2(){
-		//상단 왼쪽
-		for(int i = H1-1; i > 0; i--) arr_copy[i][0] = arr_copy[i-1][0];
-		
-		//상단 위쪽
-		for(int i = 0; i < C - 1; i++) arr_copy[0][i] = arr_copy[0][i+1];
-		
-		//상단 오른쪽
-		for(int i = 0; i < H1; i++) arr_copy[i][C-1] = arr_copy[i+1][C-1];
+	static void rotateTop(int[][] copyArr){
+		int prev = copyArr[x1][2];
+		copyArr[x1][2] = 0;
+		for(int i = 3; i <= c; i++){
+			int tmp = copyArr[x1][i];
+			copyArr[x1][i] = prev;
+			prev = tmp;
+		}
 
-		//상단 아래쪽
-		for(int i = C -1; i > 0; i--) arr_copy[H1][i] = arr_copy[H1][i-1];
+		for(int i = x1-1; i >= 1; i--){
+			int tmp = copyArr[i][c];
+			copyArr[i][c] = prev;
+			prev = tmp;
+		}
 
-		//하단 왼쪽
-		for(int i = H2+1; i < R-1; i++) arr_copy[i][0] = arr_copy[i+1][0];
+		for(int i = c - 1; i >= 1; i--){
+			int tmp = copyArr[1][i];
+			copyArr[1][i] = prev;
+			prev = tmp;
+		}
 
-		//하단 아래쪽
-		for(int i = 0; i < C-1; i++) arr_copy[R-1][i] = arr_copy[R-1][i+1];
-
-		//하단 오른쪽
-		for(int i = R-1; i > H2; i--) arr_copy[i][C-1] = arr_copy[i-1][C-1];
-
-		//하단 위쪽
-		for(int i = C-1; i > 0; i--) arr_copy[H2][i] = arr_copy[H2][i-1];
-	}
-
-	static void copy(){
-		for(int i = 0; i < R; i++){
-			for(int j = 0; j < C; j++) {
-				if(arr[i][j] == -1) continue;
-				arr[i][j] = arr_copy[i][j];
-			}
+		for(int i = 2; i < x1; i++){
+			int tmp = copyArr[i][1];
+			copyArr[i][1] = prev;
+			prev = tmp;
 		}
 	}
 
-	static void cntAns(){
-		for(int i = 0; i < R; i++){
-			for(int j = 0; j < C; j++) {
-				if(arr[i][j] == -1) continue;
-				ans += arr[i][j];
-			}
+	static void rotateBottom(int[][] copyArr){
+		int prev = copyArr[x2][2];
+		copyArr[x2][2] = 0;
+		for(int i = 3; i <= c; i++){
+			int tmp = copyArr[x2][i];
+			copyArr[x2][i] = prev;
+			prev = tmp;
+		}
+
+		for(int i = x2+1; i <= r; i++){
+			int tmp = copyArr[i][c];
+			copyArr[i][c] = prev;
+			prev = tmp;
+		}
+
+		for(int i = c-1; i>=1; i--){
+			int tmp = copyArr[r][i];
+			copyArr[r][i] = prev;
+			prev = tmp;
+		}
+
+		for(int i = r-1; i > x2; i--){
+			int tmp = copyArr[i][1];
+			copyArr[i][1] = prev;
+			prev = tmp;
 		}
 	}
 }
