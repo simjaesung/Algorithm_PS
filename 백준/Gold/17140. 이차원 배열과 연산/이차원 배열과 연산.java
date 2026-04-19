78e970411fe1dab11c2b9b32c7c20f417e52e671
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-20 01:13:26 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 188 ms, Memory: 25208 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
@@ -5,7 +5,6 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
@@ -13,66 +12,77 @@ class Main {
 		int[][] arr = new int[100][100];
 		for(int i = 0; i < 3; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < 3; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < 3; j++){
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
+	
+		int maxR = 3, maxC = 3;
+		int ans = 0;
 
-		int maxR = 3; //최대 행(가로)
-		int maxC = 3; //최대 열(세로)
-
-		int cnt = 100;
-		while(cnt-- > 0){ //100초가 지나도 
+		while(ans < 100){
 			if(arr[r-1][c-1] == k) break;
 			int[][] arrCopy = new int[100][100];
-			if(maxR >= maxC){
+			if(maxR >= maxC){ //R연산
 				for(int i = 0; i < maxR; i++){
-					Set<Integer> set = new HashSet<>();
-					Map<Integer,Integer> map = new HashMap<>();
+					Map<Integer, Integer> map = new HashMap<>();
 					for(int j = 0; j < maxC; j++){
 						if(arr[i][j] == 0) continue;
-						set.add(arr[i][j]);
-						map.put(arr[i][j], map.getOrDefault(arr[i][j],0) + 1);
+						map.put(arr[i][j], map.getOrDefault(arr[i][j], 0) + 1);
 					}
-					List<Integer> setToList = new ArrayList<>(set);
-					Collections.sort(setToList, (a,b) -> {
-						if(map.get(a) == map.get(b)) return a - b;
-						return map.get(a) - map.get(b);
+
+					List<int[]> list = new ArrayList<>();
+					for(var valCnt : map.entrySet()){
+						list.add(new int[]{valCnt.getKey(), valCnt.getValue()});
+					}
+
+					Collections.sort(list, (a,b) -> {
+						if(a[1] == b[1]) return a[0] - b[0];
+						return a[1] - b[1];
 					});
 
-					int len = Math.min(50, setToList.size());
-					maxC = Math.max(len * 2, maxC);
-					for(int l = 0; l < len * 2; l+=2){
-						arrCopy[i][l] = setToList.get(l / 2);
-						arrCopy[i][l+1] = map.get(setToList.get(l / 2));
+					int len = Math.min(50, list.size());
+					for(int l = 0; l < len; l++){
+						arrCopy[i][l * 2] = list.get(l)[0];
+						arrCopy[i][l * 2+1] = list.get(l)[1];
 					}
+					
+					maxC = Math.max(maxC, len * 2);
 				}
-			}else{
+			}else{ //C연산
 				for(int i = 0; i < maxC; i++){
-					Set<Integer> set = new HashSet<>();
-					Map<Integer,Integer> map = new HashMap<>();
-
+					Map<Integer, Integer> map = new HashMap<>();
 					for(int j = 0; j < maxR; j++){
 						if(arr[j][i] == 0) continue;
-						set.add(arr[j][i]);
-						map.put(arr[j][i], map.getOrDefault(arr[j][i],0) + 1);
+						map.put(arr[j][i], map.getOrDefault(arr[j][i], 0) + 1);
 					}
 
-					List<Integer> setToList = new ArrayList<>(set);
-					Collections.sort(setToList, (a,b) -> {
-						if(map.get(a) == map.get(b)) return a - b;
-						return map.get(a) - map.get(b);
+					List<int[]> list = new ArrayList<>();
+					for(var valCnt : map.entrySet()){
+						list.add(new int[]{valCnt.getKey(), valCnt.getValue()});
+					}
+
+					Collections.sort(list, (a,b) -> {
+						if(a[1] == b[1]) return a[0] - b[0];
+						return a[1] - b[1];
 					});
 
-					int len = Math.min(50, setToList.size());
-					maxR = Math.max(maxR, len * 2);
-					for(int l = 0; l < len * 2; l+=2){
-						arrCopy[l][i] = setToList.get(l / 2);
-						arrCopy[l+1][i] = map.get(setToList.get(l / 2));
+					int len = Math.min(50, list.size());
+					for(int l = 0; l < len; l++){
+						arrCopy[l * 2][i] = list.get(l)[0];
+						arrCopy[l * 2+1][i] = list.get(l)[1];
 					}
+					maxR = Math.max(maxR, len * 2);
 				}
 			}
-			arr = arrCopy;
+
+			for(int i = 0; i < 100; i++){
+				for(int j = 0; j < 100; j++) arr[i][j] = arrCopy[i][j];
+			}
+			ans++;
 		}
 
-		System.out.println(arr[r-1][c-1] == k ? 100 - cnt - 1 : -1);
+		if(arr[r-1][c-1] != k) System.out.println(-1);
+		else System.out.println(ans);
 	}
 }
