d5826902bc939f6c854cdef65c971a972ec020d0
jaesung sim <89987940+simjaesung@users.noreply.github.com>
2026-04-23 08:23:50 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	//0:북,1:동,2:남,3:서
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -9,53 +13,56 @@ class Main {
 		int m = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
-		int cx = Integer.parseInt(st.nextToken());
-		int cy = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(st.nextToken());
-
-		int[] dx = {-1,0,1,0};
-		int[] dy = {0,1,0,-1};
 
 		int[][] arr = new int[n][m];
 		for(int i = 0; i < n; i++){
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < m; j++) arr[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
-		//1:벽, 0:빈칸, -1:청소
+
 		int ans = 0;
-		while(true){
+		Deque<int[]> dq = new ArrayDeque<>();
+		dq.addLast(new int[]{r,c});
+
+		while(!dq.isEmpty()){
+			int[] cur = dq.removeFirst();
+			int cx = cur[0], cy = cur[1];
+			
+			//현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
 			if(arr[cx][cy] == 0){
 				ans++; 
-				arr[cx][cy] = -1;
+				arr[cx][cy] = 2;
 			}
 
-			boolean isDirty = false;
-			
+			boolean isNotClear = false;
+			//현재 칸의 주변 4칸 중 청소되지 않았는지
 			for(int i = 0; i < 4; i++){
 				int nx = cx + dx[i];
 				int ny = cy + dy[i];
-				if(arr[nx][ny] == 0) {
-					isDirty = true;
-					break;
-				}
+				if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+				if(arr[nx][ny] == 0) isNotClear = true;
 			}
 
-			if(isDirty){
-				//청소되지 않은 빈 칸이 있는 경우
-				d = (d + 4 - 1) % 4;
+			//청소되지 않은 빈 칸이 있는 경우
+			if(isNotClear){
+				//반시계 회전
+				d = (d - 1 + 4) % 4;
 				int nx = cx + dx[d];
 				int ny = cy + dy[d];
-				if(arr[nx][ny] == 0) {
-					cx = nx; 
-					cy = ny;
-				}
+				if(arr[nx][ny] == 0) dq.addLast(new int[]{nx,ny});
+				else dq.addLast(new int[]{cx,cy});
 			}else{
 				//청소되지 않은 빈 칸이 없는 경우
-				int back = (d + 2) % 4;
-				cx += dx[back]; cy += dy[back];
-				if(arr[cx][cy] == 1) break;
+				//방향 유지, 한 칸 후진하고 돌아간다
+				int nx = cx + dx[(d + 2) % 4];
+				int ny = cy + dy[(d + 2) % 4];
+				if(arr[nx][ny] == 1) break;
+				dq.addLast(new int[]{nx,ny});
 			}
+			
 		}
 		
 		System.out.println(ans);
